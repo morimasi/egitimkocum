9e42f7d1b4b8dbe90134bd0a89c80539564c3b4d
morimasi <99003675+morimasi@users.noreply.github.com>
2025-10-13 19:38:14 +0300

    refactor: Remove unused placeholder functions
    Deletes the placeholder serverless function and its associated package.json. These files were added to prevent build errors but are no longer needed as the functions directory will be properly populated.

--- functions/index.js
+++
@@ -1,5 +0,0 @@
-// This file is a placeholder to prevent Vercel build errors.
-// It creates a valid, but unused, serverless function.
-export default (req, res) => {
-  res.status(200).send('This function is not in use.');
-};
